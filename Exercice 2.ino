c7c144464aa9cf890ec8ebfb3b8854766ef87105
Nokami <77856367+tkyoangel@users.noreply.github.com>
2022-09-22 11:57:02 +0200

    Update Exercice 2.ino

--- Exercice 2.ino
+++ Exercice 2.ino
@@ -3,10 +3,10 @@ void setup(){
 }
 
 void loop(){
-    int nombre;
+    float nombre;
     if (Serial.available()){
         nombre = Serial.parseInt ();
         nombre = sq(nombre);
         Serial.println(nombre);
     }
-} 
+}
