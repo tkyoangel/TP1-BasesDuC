2254e0fdbb1c82df7932b6224576481aae9b980f
Nokami <77856367+tkyoangel@users.noreply.github.com>
2022-09-22 11:49:00 +0200

    Update Exercice 1.ino

--- Exercice 1.ino
+++ Exercice 1.ino
@@ -1,12 +1,16 @@
 int i;
 int k;
 void setup(){
-
+  Serial.begin(9600);
 }
 
 void loop(){
     for (i=0; i<10; i++){
-        for (k=0; i<10; i++){
+        for (k=0; k<10; k++){
+            Serial.print(i);
+            Serial.print("x");
+            Serial.print(k);
+            Serial.print("=");
             Serial.println(k*i);
         }
     }
